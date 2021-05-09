1f98534dc9c5b12650e25cf0f3479857eb3447d4
AashuGaming <77149352+AashuGaming@users.noreply.github.com>
2021-05-09 15:07:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Firebase from "firebase"
+
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+
+  firebase.auth().signOut().then(() => {
+    // Sign-out successful.
+  }).catch((error) => {
+    // An error happened.
+  });
